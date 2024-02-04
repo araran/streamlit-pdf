2bc8ada10beebf345c7136eaf92684ec9c8ddebd
araran <araitomonori@gmail.com>
2024-02-04 05:15:54 +0000

    再起動

--- Hello.py
+++ Hello.py
@@ -1,22 +1,33 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+import os
 import streamlit as st
 from streamlit.logger import get_logger
+import google.generativeai as genai
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain import hub
+from langchain.agents import AgentExecutor, create_json_chat_agent
+
+import pypdf
 
 LOGGER = get_logger(__name__)
+# Gemini APIキーの設定
+genai.configure(api_key="AIzaSyBagYfwNZdCl-0yGMX1NvHuZelaC4SAZFg")
 
+# Langsmith APIキーの設定
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
+os.environ["LANGCHAIN_API_KEY"] = "ls__14d9d98ac88c42f69d2bc7a018792351"
+os.environ["LANGCHAIN_PROJECT"] = "souzoku"
+
+# Tavily  APIキーの設定
+os.environ["TAVILY_API_KEY"] = "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"
+
+# ツールの準備
+tools = [TavilySearchResults(max_results=1)]
+# プロンプトの準備
+prompt = hub.pull("hwchase17/react-chat-json")
+model = genai.GenerativeModel('gemini-pro')
+# Construct the ReAct agent
+agent = create_json_chat_agent(model, tools, prompt)
 
 def run():
     st.set_page_config(
@@ -26,25 +37,47 @@ def run():
 
     st.write("# Welcome to Streamlit! 👋")
 
-    st.sidebar.success("Select a demo above.")
+    st.sidebar.success("上のデモを選択してください。")
 
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
+         Streamlit は、以下のために特別に構築されたオープンソース アプリ フレームワークです。
+        """
     )
+
+
+    pdf_template = "法定相続情報一覧図の保管及び交付の申出書.pdf"
+    pdf_output = "output.pdf" 
+
+    pdf = pypdf.PdfReader(pdf_template)
+#   pdfWriter = PyPDF2.PdfFileWriter()
+
+    #st.write(pdf.get_fields())
+
+    # フォームのフィールドに値を設定
+    #pdfWriter.getFields()["name"] = "Taro Yamada"  
+    #pdfWriter.getFields()["address"] = "Tokyo, Japan"  
+
+    #pdfOutput = open(pdf_output, 'wb')
+    #pdfWriter.write(pdfOutput)
+
+
+    # Streamlitのインターフェース設定
+    st.title('😱Generative AI with Google API')
+    user_input = st.text_input("質問を入力してください:")
+
+    if user_input:
+        # モデルの設定
+
+        # Create an agent executor by passing in the agent and tools
+        agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True)
+        # ユーザーの入力をモデルに渡す
+
+        result = agent_executor.invoke({"input": user_input})
+
+        # 結果を表示
+        st.write(result)
+
 
 
 if __name__ == "__main__":
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Welcome to streamlit
 
-This is the app you get when you run `streamlit hello`, extracted as its own app.
+これは、「streamlit hello」を実行すると取得されるアプリで、独自のアプリとして抽出されます。
 
-Edit [Hello.py](./Hello.py) to customize this app to your heart's desire. ❤️
+[Hello.py](./Hello.py) を編集して、このアプリを思いどおりにカスタマイズします。 ❤️
 
-Check it out on [Streamlit Community Cloud](https://st-hello-app.streamlit.app/)
+【Streamlit Community Cloud】(https://st-hello-app.streamlit.app/)で確認してください。
